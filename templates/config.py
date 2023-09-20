dd030e4131f2f96dac8416cc31686104cf45f3d4
Yeohks217 <145543073+Yeohks217@users.noreply.github.com>
2023-09-20 20:51:28 +0800

    Add files via upload

---
+++ templates/config.py
@@ -0,0 +1,7 @@
+customhost = "taruc-database.cuodcui1nj3v.us-east-1.rds.amazonaws.com"
+customuser = "admin"
+custompass = "admin123456"
+customdb = "taruc"
+custombucket = "leesiaojuin-bucket"
+customregion = "us-east-1"
+
